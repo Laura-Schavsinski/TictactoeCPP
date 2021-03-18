b2901653dc15381b8b341d9107428329ea9de436
Laura-Schavsinski <l.schavsinski@rns.ecolescreatives.com>
2021-03-18 15:36:53 +0100

    Correction
    jjgygiyt

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -3,22 +3,44 @@
 
 #include "Grille.h"
 #include "Tictactoe.h"
+#include <iostream>
 
+    Tictactoe::Tictactoe() : _symboleCourant ('X')
 
-    Tictactoe::Tictactoe(){}
+    }
 
-    void Tictactoe::afficheGrille(){}
+    void Tictactoe::afficheGrille(){
+        _grilleDeJeu.affiche();
+    }
 
-    void Tictactoe::ajouteSymbole(int x, int y){}
+    void Tictactoe::ajouteSymbole(int x, int y){
+        _grilleDeJeu.setContent(x,y,_symboleCourant);
+    }
 
-    bool Tictactoe::testeVictoireVerticale(){}
+    bool Tictactoe::testeVictoireVerticale(){
+        for(int i=0;1<2;1++){
+        if(_grilleDeJeu.getContent(0,0)==_grilleDeJeu.getContent(1,0)
+        && _grilleDeJeu.getContent(0,0)==_grilleDeJeu.getContent(2,0)
+        && -_grilleDeJeu.getContent(0,0)!' ')
+        {return true;}
+        }
+        return : false;
+    }
 
-    bool Tictactoe::testeVictoireHorizontale(){}
+    bool Tictactoe::testeVictoireHorizontale(){
+        return : false
+    }
 
-    bool Tictactoe::testeVictoireDiagonale(){}
+    bool Tictactoe::testeVictoireDiagonale(){
 
-    bool Tictactoe::testeJeuNul(){}
+    }
 
-    void Tictactoe::finTour(){}
+    bool Tictactoe::testeJeuNul(){
+
+    }
+
+    void Tictactoe::finTour(){
+     _symboleCourant== ‘X’?_symboleCourant =‘O’:_symboleCourant =’X’;
+    }
 
 #endif
